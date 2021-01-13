31d083259beea8cc1f9c158b681090826445e80b
jslee1992@gmail.com <jslee1992@gmail.com>
2021-01-13 00:33:16 -0500

    public picture assets added

--- src/containers/ImageCarousel.js
+++ src/containers/ImageCarousel.js
@@ -1,16 +1,26 @@
 import '../App.css';
 import React from 'react';
 
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Container } from 'react-bootstrap';
 
 class ImageCarousel extends React.Component {
+
 	render(){
 		return (
-			<Carousel>
-				<Carousel.Item>
-					<img src='/logo-text.png'/>
-				</Carousel.Item>	
-			</Carousel>
+			<div>
+				<img src='/dsc1.jpg' style={{'height':"300px"}}/>
+				<Carousel height='100px' showControls={true} >
+					<Carousel.Item style={{'height':"300px"}}>
+						<img src='/logo-text.png'/>
+					</Carousel.Item >	
+					<Carousel.Item style={{'height':"300px"}}>
+						<img src='/dsc1'/>
+					</Carousel.Item >
+					<Carousel.Item style={{'height':"300px"}}>
+						<img src='/dsc3.jgp'/>
+					</Carousel.Item>
+				</Carousel>
+			</div>
 		)
 	}
 }
